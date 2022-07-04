081c81389c9e19338aa3a8d50c0810d8b7f84622
Dener <dener@startapp.one>
2022-07-04 18:53:00 -0300

    a

---
+++ src/shared/typeorm/migrations/1656971299892-Teste.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Teste1656971299892 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
