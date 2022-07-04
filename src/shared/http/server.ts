4f442adebf346fa0972f7c9de7fea8478768f639
Dener <dener@startapp.one>
2022-07-04 18:47:06 -0300

    v3

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,16 +1,19 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '../errors/AppError';
 import '../typeorm';
 
 const app = express();
-
+app.listen(3333, () => {
+  global.console.log('Server started on port 3333');
+});
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
+
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
@@ -25,6 +28,3 @@ app.use(
     }
   },
 );
-app.listen(3333, () => {
-  global.console.log('Server started on port 3333');
-});
